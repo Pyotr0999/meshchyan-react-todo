ab8036ba06701f9570a2d2136c3c166638eb680f
Pyotr0999 <sergaghasaryan@gmail.com>
2025-04-12 11:48:17 +0400

    reducer-todocer-finishcer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 import TodoFooter from './TodoFooter';
-
+function reducer(state,action) {
+  if (action.type === "add") {
+    return [
+      ...state,
+      {
+        id:Math.random(),
+        text: action.payload.text,
+        isCompleted: false
+      }
+    ]
+  }else if(action.type === "delet"){
+    return state.filter((t) => t.id !== action.payload.id)
+  }
+}
+// function useReducer(reducer,inityolState) {
+//   const [state, setState] = useState(inityolState)
+//   return [state, (action) => {
+//      setState(newState)
+//   }]
+// }
 function App() {
-  const [todos, setTodos] = useState([
+ const [todos,dispach] = useReducer(reducer,[
     {
       id:Math.random(),
       text: "Learn JS",
@@ -25,19 +44,24 @@ function App() {
   return (
     <div className="App">
       <TodoForm onAdd={(text) => {
-        setTodos([
-          ...todos,
-          {
-            id:Math.random(),
-            text: text,
-            isCompleted:false
-          }
-        ])
+        dispach({
+          type: 'add',
+          payload: {
+          text: text
+        }
+        })
+       
       }}/>
       <TodoList
        todos={todos}
        onDelete={(todo)=>{
-        setTodos(todos.filter((t)=> t.id !== todo.id))
+        dispach({
+          type: "delet",
+          payload: {
+            id:todo.id
+          }
+        })
+        // setTodos(todos.filter((t)=> t.id !== todo.id))
        }}
        onChange={(newTodo)=>{
         setTodos(todos.map((todo) => {
